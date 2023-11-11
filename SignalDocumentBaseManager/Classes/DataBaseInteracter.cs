0ddb24f8b0d6117f2f4464545375e0820c050bd2
artemef1996@gmail.com <artemef1996@gmail.com>
2023-11-11 20:12:27 +0300

    Logic is separeted from code behind

---
+++ SignalDocumentBaseManager/Classes/DataBaseInteracter.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SignalDocumentBaseManager.Classes
+{
+    static public class DataBaseInteracter
+    {
+        static public async Task<DocumentFile[]> GetDocumentsAsync()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var result = await client.GetAsync("https://localhost:7231/Documents").ConfigureAwait(false);
+
+                return await result.Content.ReadFromJsonAsync<DocumentFile[]>();
+            }
+        }
+
+        static public async Task PostDocumentsAsync(DocumentFile documentToPost)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var json = JsonConvert.SerializeObject(documentToPost);
+
+                string param = $"json={json}";
+
+                var content = new StringContent(param, Encoding.UTF8, "application/json");
+
+                string ct = content.ToString();
+
+                try
+                {
+                    var result = await client.PostAsync("https://localhost:7231/Documents?" + param, content);
+
+                    result.EnsureSuccessStatusCode();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Exception heppend: " + ex);
+                }
+            }
+        }
+
+    }
+}
---
+++ SignalDocumentBaseManager/Classes/DocumentFile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalDocumentBaseManager.Classes
+{
+    public class DocumentFile
+    {
+        public int Id { get; set; }
+
+        public string Type { get; set; }
+
+        public string Name { get; set; }
+
+        public string Number { get; set; }
+
+        public string ReleaseDate { get; set; }
+
+        public string EntryDate { get; set; }
+
+        public string KeyWords { get; set; }
+
+        public int AccessLevel { get; set; }
+    }
+}
